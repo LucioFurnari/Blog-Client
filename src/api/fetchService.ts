d89291c3c8a9b05d1cc286f2b73143c65b1b3b8e
LucioFurnari <luciofurnari99@gmail.com>
2024-02-29 20:12:12 -0300

    Create postLoginUser function to get user data of the api

--- src/api/fetchService.ts
+++ src/api/fetchService.ts
@@ -11,3 +11,15 @@ export async function getUserData (token: string) {
 
   return res;
 }
+
+export async function postLoginUser(userLoginInfo: object) {
+  const res = await fetch('http://localhost:3000/api/login', {
+    method: 'POST',
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    }),
+    body: JSON.stringify(userLoginInfo),
+  });
+
+  return res;
+}
