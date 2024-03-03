51f8257eba70c4fb4196aa9d6f987eee737b9d0e
LucioFurnari <luciofurnari99@gmail.com>
2024-03-03 12:49:03 -0300

    Import getPosts function

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,26 +1,16 @@
 import PostCard from "./PostCard";
+import { getPosts } from "@/api/fetchService";
 
-async function getPosts() {
-  const res = await fetch('http://localhost:3000/api/posts', {
-    next: {revalidate: 10},
-    method: 'GET',
-    headers: new Headers({
-      'Content-Type': 'application/json'
-    })
-  });
-
-  const data = await res.json();
-  return data;
-}
 
 export default async function Main () {
-  const data = await getPosts();
+  const posts = await getPosts()
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1>This is the home</h1>
       <section className=" w-full grid grid-cols-4 grid-rows-[300px_300px_300px] gap-4">
       {
-        data.map((item: {
+        posts.map((item: {
           _id: string,
           title: string,
           text: string,
