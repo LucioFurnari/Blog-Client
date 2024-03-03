980c5d66aa2a61e0070b0465ddf0a424fb2b1a48
LucioFurnari <luciofurnari99@gmail.com>
2024-03-02 23:47:24 -0300

    Move getPosts function trom the Main component

--- src/api/fetchService.ts
+++ src/api/fetchService.ts
@@ -35,3 +35,16 @@ export async function getSession(token: string) {
 
   return res;
 }
+
+export async function getPosts() {
+  const res = await fetch('http://localhost:3000/api/posts', {
+    next: {revalidate: 10},
+    method: 'GET',
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  });
+
+  const data = await res.json();
+  return data;
+}
