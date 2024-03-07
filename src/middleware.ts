f371b851e9501ab7582bb1477ad970a3c742873b
LucioFurnari <luciofurnari99@gmail.com>
2024-03-07 20:58:14 -0300

    Change middleware function to use cookies, and response status code

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,19 @@ import { NextResponse } from "next/server";
 import { getSession } from "@/api/fetchService";
 
 export async function middleware(request: NextRequest) {
-  // const token = localStorage.getItem('token');
-  const token = null;
-  console.log('Test')
-  if (token === null) {
-    return Response.redirect(new URL('/login', request.url));
-  }
+    
+    const token = request.cookies.get('token')?.value
+
+    if (token === undefined) {
+      return Response.redirect(new URL('/login', request.url));
+    }
+
+    const res = await getSession(token);
+    const data = await res?.json();
+
+    if (!res?.ok) {
+      return Response.redirect(new URL('/', request.url));
+    }
 }
 
 export const config = {
