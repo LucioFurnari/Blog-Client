0ee653f514143acf4fbe78282a3447317ea60e5e
LucioFurnari <luciofurnari99@gmail.com>
2024-03-09 22:28:06 -0300

    Create Post component

---
+++ src/components/Post.tsx
@@ -0,0 +1,18 @@
+import CommentsContainer from "./CommentsContainer";
+
+export default function PostPage (props: {
+  title: string,
+  author: string,
+  text: string,
+},
+  id: string
+) {
+  return (
+    <main>
+    <h1>{props.title}</h1>
+    <p>{props.author}</p>
+    <p>{props.text}</p>
+    <CommentsContainer postId={id}/>
+  </main>
+  )
+}
