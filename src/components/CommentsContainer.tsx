ea4b8acf39bde902b0ffd1e8d21ecd14dddaee9f
LucioFurnari <luciofurnari99@gmail.com>
2024-03-09 22:12:46 -0300

    Create Comments container component

---
+++ src/components/CommentsContainer.tsx
@@ -0,0 +1,21 @@
+import { getComments } from "@/api/fetchService";
+import Comment from "./Comment";
+
+export default async function CommentsContainer (props: {postId: string}) {
+  const comments = await getComments(props.postId);
+
+  return (
+    <div>
+    {
+      comments.map((comment: {
+        _id: string,
+        author: string,
+        text: string,
+        timestamp: string,
+      }) => {
+        return (<Comment key={comment._id} {...comment} />)
+      })
+    }
+    </div>
+  )
+}
