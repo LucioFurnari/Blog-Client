86e709e82dda6f3e9316bafe98c83a3549d0c052
LucioFurnari <luciofurnari99@gmail.com>
2024-03-11 19:01:53 -0300

    Remove functions and import LoginForm component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,43 +1,10 @@
-
-import { redirect } from "next/navigation";
-import { postLoginUser } from "@/api/fetchService";
-import { setToken } from "./actions";
-
-export async function setLocalStorage (token: string) {
-  if (localStorage.getItem('token') === 'null') {
-    localStorage.setItem('token', token);
-  } else {
-    localStorage.setItem('token', token)
-  }
-};
-
+import LoginForm from "@/components/LoginForm";
 
 export default function LoginPage () {
 
-  async function handleSubmit (formData: FormData) {
-    'use server'
-
-    const rawFormData = {
-      username: formData.get('username'),
-      password: formData.get('password')
-    }
-    const res = await postLoginUser(rawFormData);
-
-    if (!res.ok) throw res;
-
-    const data = await res.json();
-    setToken(data.token)
-    redirect('/')
-  };
-
   return (
     <main>
-      <form action={handleSubmit}>
-        <label htmlFor="username">Username</label>
-        <input className="text-black" name="username" id="username" type="text" placeholder="Username"></input>
-        <input className="text-black" name="password" type="password" placeholder="Password"></input>
-        <button type="submit">Login</button>
-      </form>
+      <LoginForm />
     </main>
   )
 };
