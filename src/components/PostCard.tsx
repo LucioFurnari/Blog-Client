c407481b61751d959f85c8076cd8ed4a2da657fb
LucioFurnari <luciofurnari99@gmail.com>
2024-03-10 23:26:59 -0300

    Add Link next component

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 interface PostCardProps {
   _id: string,
   title: string,
@@ -8,7 +10,8 @@ interface PostCardProps {
 
 export default function PostCard (props: PostCardProps) {
   return (
-    <div className=" text-white border-2 border-white p-4 overflow-hidden">
+  <Link href={`/post/${props._id}`}>
+    <div className="text-white border-2 border-white p-4 overflow-hidden h-full">
       <div className="flex flex-row justify-between">
         <h2 className=" text-xl">{props.title}</h2>
         <span>{props.timestamp}</span>
@@ -16,5 +19,6 @@ export default function PostCard (props: PostCardProps) {
       <h3 className=" mb-4">{props.author}</h3>
       <p className=" line-clamp-[8]">{props.text}</p>
     </div>
+  </Link>
   );
 };
