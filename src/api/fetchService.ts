45624c20006f465678fc747fc8c48568fe8915f3
LucioFurnari <luciofurnari99@gmail.com>
2024-03-11 17:39:22 -0300

    Create enum to endpoints and add comments

--- src/api/fetchService.ts
+++ src/api/fetchService.ts
@@ -1,6 +1,13 @@
+enum Endpoints {
+  Login = 'login',
+  Session = 'session',
+  Posts = 'posts',
+  Comments = 'comments'
+}
 
+// Function to login user
 export async function postLoginUser(userLoginInfo: object) {
-  const res = await fetch('http://localhost:3000/api/login', {
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URL_DEV+Endpoints.Login, {
     method: 'POST',
     headers: new Headers({
       'Content-Type': 'application/json'
@@ -11,9 +18,10 @@ export async function postLoginUser(userLoginInfo: object) {
   return res;
 }
 
+// Function to get user session data
 export async function getSession(token: string) {
   try {
-    const res = await fetch('http://localhost:3000/api/session', {
+    const res = await fetch(process.env.NEXT_PUBLIC_API_URL_DEV+Endpoints.Session, {
       method: 'GET',
       headers: new Headers({
         'Content-Type': 'application/json',
@@ -27,9 +35,10 @@ export async function getSession(token: string) {
   }
 }
 
+// Function to get all posts
 export async function getPosts() {
   try {
-    const res = await fetch('http://localhost:3000/api/posts', {
+    const res = await fetch(process.env.NEXT_PUBLIC_API_URL_DEV+Endpoints.Posts, {
     // next: {revalidate: 10},
     method: 'GET',
     headers: new Headers({
@@ -50,6 +59,7 @@ export async function getPosts() {
   }
 }
 
+// Function to get a single post
 export async function getPost(postId: string) {
   try {
     const res = await fetch(`http://localhost:3000/api/posts/${postId}`, {
@@ -67,13 +77,15 @@ export async function getPost(postId: string) {
   }
 }
 
+// Function to get all the comments of a post
 export async function getComments(postId: string) {
   try {
     const res = await fetch(`http://localhost:3000/api/posts/${postId}/comments`, {
       method: 'GET',
       headers: new Headers({
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       }),
+      cache: 'no-store'
     });
     
     const data = await res.json();
