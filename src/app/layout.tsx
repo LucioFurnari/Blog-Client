a33036b791aac8d9dec2b6c20baf5eaaeac86c27
LucioFurnari <luciofurnari99@gmail.com>
2024-03-12 22:39:30 -0300

    Import navbar and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "./context/UserContext";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <UserProvider>
+          <Navbar />
           {children}
+          <Footer />
         </UserProvider>
       </body>
     </html>
