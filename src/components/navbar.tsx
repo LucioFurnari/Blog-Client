d2c0cf919bfa2a83527aedf04b833239b9f0af44
LucioFurnari <luciofurnari99@gmail.com>
2024-03-12 22:39:51 -0300

    Use useContext UserContext

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,44 +1,22 @@
 'use client'
 
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { getSession } from "@/api/fetchService";
-import { getToken } from "./actions";
+import { useContext } from "react";
+import { deleteToken } from "./actions";
+import { UserContext } from "@/app/context/UserContext";
 
 export default function Navbar () {
-  const [user, setUser] = useState('');
-
-  useEffect(() => {
-    const listenStorageChange = async () => {
-      try {
-        const token = await getToken();
-        if (token !== undefined) {
-        const res = await getSession(token);
-
-        //todo: Change this
-        if (typeof res === 'undefined') {
-          throw new Error('Server not working')
-        }
-        if (res.ok) {
-          const data = await res.json()
-          console.log(data)
-          setUser(data.username)
-        }
-        }
-      } catch (error) {
-        console.error(error)
-      }
-    }
-      listenStorageChange()
-  }, [])
-
+  const { user } = useContext(UserContext)
   return (
     <nav className=" flex justify-around">
       <Link className=" p-6 hover:text-cyan-300" href={"/"}>Home</Link>
       <div className="flex flex-row">
       {
         user !== '' ?
+        <>
         <p className="p-6">{user}</p> 
+        <button onClick={ async () => await deleteToken()}>Logout</button>
+        </>
         :
         <>
         <Link className="p-6 hover:text-cyan-300"href={"/login"}>Login</Link>
