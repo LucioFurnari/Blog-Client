29e7b48a7b01cd0a34d0f65069476a58abf6f29e
LucioFurnari <luciofurnari99@gmail.com>
2024-03-13 22:48:05 -0300

    Move and create new function to set and get token

--- src/components/actions.ts
+++ src/components/actions.ts
@@ -1,6 +1,16 @@
 'use server'
 
+import { postLoginUser } from "@/api/fetchService";
 import { cookies } from "next/headers"
+import { redirect } from "next/navigation";
+
+async function setToken(token: string) {
+  cookies().set({
+    name: 'token',
+    value: token,
+    httpOnly: true,
+  })
+}
 
 export async function getToken() {
   const cookieStore = cookies();
@@ -8,3 +18,23 @@ export async function getToken() {
 
   return token;
 }
+
+export async function deleteToken() {
+  cookies().delete('token');
+}
+
+export async function handleLogin (formData: FormData) {
+  'use server'
+
+  const rawFormData = {
+    username: formData.get('username'),
+    password: formData.get('password')
+  }
+  const res = await postLoginUser(rawFormData);
+
+  if (!res.ok) throw res;
+
+  const data = await res.json();
+  setToken(data.token)
+  redirect('/')
+};
