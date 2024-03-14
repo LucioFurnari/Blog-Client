eeaa5e3ee7518c3265ae74852c02db066117c9a3
LucioFurnari <luciofurnari99@gmail.com>
2024-03-13 22:49:36 -0300

    Use Usercontext, remove useEffect

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,23 +1,26 @@
 'use client'
 
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { deleteToken } from "./actions";
 import { UserContext } from "@/app/context/UserContext";
 
 export default function Navbar () {
   const { user } = useContext(UserContext)
+
   return (
     <nav className=" flex justify-around">
       <Link className=" p-6 hover:text-cyan-300" href={"/"}>Home</Link>
       <div className="flex flex-row">
       {
-        user !== '' ?
+        user &&
         <>
         <p className="p-6">{user}</p> 
         <button onClick={ async () => await deleteToken()}>Logout</button>
         </>
-        :
+      }
+      {
+        !user &&
         <>
         <Link className="p-6 hover:text-cyan-300"href={"/login"}>Login</Link>
         <Link className="p-6 hover:text-cyan-300" href={"/signup"}>Signup</Link>
