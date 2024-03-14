7b5714970849d921d940bc116becd7c982db1aca
LucioFurnari <luciofurnari99@gmail.com>
2024-03-13 23:40:03 -0300

    Import and use deleteUser function

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,25 +6,35 @@ import { deleteToken } from "./actions";
 import { UserContext } from "@/app/context/UserContext";
 
 export default function Navbar () {
-  const { user } = useContext(UserContext)
+  const { user, deleteUser, isLogged } = useContext(UserContext)
+
+  function handleDeleteUser () {
+    deleteToken();
+    deleteUser()
+  }
 
   return (
     <nav className=" flex justify-around">
       <Link className=" p-6 hover:text-cyan-300" href={"/"}>Home</Link>
       <div className="flex flex-row">
-      {
-        user &&
+      
+      {isLogged && (
         <>
-        <p className="p-6">{user}</p> 
-        <button onClick={ async () => await deleteToken()}>Logout</button>
+        {user &&
+        <>
+          <p className="p-6">{user}</p> 
+          <button onClick={ handleDeleteUser }>Logout</button>
         </>
-      }
-      {
+        }
+        {
         !user &&
         <>
         <Link className="p-6 hover:text-cyan-300"href={"/login"}>Login</Link>
         <Link className="p-6 hover:text-cyan-300" href={"/signup"}>Signup</Link>
         </>
+        }
+        </>
+      )
       }
       </div>
     </nav>
