00dd031f008b06eb0426768d09c395ee5816aae7
LucioFurnari <luciofurnari99@gmail.com>
2024-03-16 23:58:03 -0300

    Minort changes

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,13 +1,19 @@
+
 export default function Comment (commentProps: {
+  _id: string,
   author: string,
   text: string,
   timestamp: string,
+  response_to: string,
 }) {
+
   return (
-    <div>
-      <span>{commentProps.author}</span>
+    <div className=" border-2 border-red-400">
+      <div>
+        <span className="mr-10">{commentProps.author}</span>
+        <span>{commentProps.timestamp}</span>
+      </div>
       <p>{commentProps.text}</p>
-      <p>{commentProps.timestamp}</p>
     </div>
   )
 }
