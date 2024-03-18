9237e14655a80a6eea1d0be7863e13df361bd5e9
LucioFurnari <luciofurnari99@gmail.com>
2024-03-17 23:44:35 -0300

    Change returned component

--- src/components/CommentList.tsx
+++ src/components/CommentList.tsx
@@ -10,17 +10,19 @@ interface CommentProp {
 
 
 export function CommentList (props: { comments: CommentProp[], group: any, getReplies: any }) {
-  return props.comments.map((comment: {
-    _id: string,
-    author: string,
-    text: string,
-    timestamp: string,
-    response_to: string
-  }) => {
-    return (
-      <div key={comment._id}>
-        <Comment {...comment} group={props.group} getReplies={props.getReplies} />
-      </div>
-    )
-  })
+  return (
+    props.comments.map((comment: {
+      _id: string,
+      author: string,
+      text: string,
+      timestamp: string,
+      response_to: string
+    }) => {
+      return (
+        <div key={comment._id}>
+          <Comment {...comment} group={props.group} getReplies={props.getReplies} />
+        </div>
+      )
+    })
+  )
 }
