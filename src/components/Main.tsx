c0a901efd54a96546df420011ab271e3cd533edd
LucioFurnari <luciofurnari99@gmail.com>
2024-03-21 16:25:19 -0300

    Import PostCardContainer, remove foreach iteration

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import PostCard from "./PostCard";
+import PostCardContainer from "./PostCardContainer";
 import { getPosts } from "@/api/fetchService";
 
 export default async function Main () {
@@ -7,24 +7,7 @@ export default async function Main () {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1>This is the home</h1>
-      <section className=" w-full grid grid-cols-4 grid-rows-[300px_300px_300px] gap-4">
-      {
-        posts.length > 0 ?
-        posts.map((post: {
-          _id: string,
-          title: string,
-          text: string,
-          author: string,
-          timestamp: string
-        }, index: number) => {
-          return (
-              <PostCard key={post._id} {...post} />
-          )
-        })
-        : 
-        <div>Post not found</div>
-      }
-      </section>
-  </main>
+      <PostCardContainer posts={posts} />
+    </main>
   )
 };
