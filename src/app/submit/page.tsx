35452bc7756185367ad8f5fb9037b954a25f6e39
LucioFurnari <luciofurnari99@gmail.com>
2024-03-22 23:22:11 -0300

    Add tiptap form

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -2,16 +2,29 @@
 
 import { useEditor, EditorContent } from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
+import { useState } from "react";
 
 export default function Tiptap () {
+  const [editorContent, setEditorContent] = useState('')
   const editor = useEditor({
     extensions: [
-      StarterKit,
+      StarterKit.configure({
+        heading: {
+          levels: [1, 2, 3],
+        }
+      }),
     ],
-    content: '<p>Hello World! 🌎️</p>',
+    content: '',
+    onUpdate({ editor }) {
+      setEditorContent(editor.getText());
+      console.log(editor.getJSON());
+    }
   });
 
   return (
+    <>
     <EditorContent editor={editor} />
+    <p>{editorContent}</p>
+    </>
   )
 }
