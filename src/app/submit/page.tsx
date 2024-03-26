ea85deb860db31dfa97c999e62c0d53d1653829f
LucioFurnari <luciofurnari99@gmail.com>
2024-03-26 18:13:43 -0300

    Remove content and import SubmitPostPage

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -1,56 +1,9 @@
 'use client'
 
-import { useEditor, EditorContent } from "@tiptap/react"
-import StarterKit from "@tiptap/starter-kit"
-import { useEffect, useState } from "react";
+import SubmitPostPage from "@/components/SubmitPostPage"
 
-export default function Tiptap () {
-  const [editorContent, setEditorContent] = useState('')
-  const editor = useEditor({
-    extensions: [
-      StarterKit.configure({
-        heading: {
-          levels: [1, 2, 3],
-        }
-      }),
-    ],
-    onUpdate({ editor }) {
-      setEditorContent(editor.getText());
-      console.log(editor.getJSON());
-    }
-  });
-  useEffect(() => {
-    editor?.commands.setContent([
-      {
-        "type": "paragraph",
-        "content": [
-          {
-            "type": "text",
-            "text": "It’s 19871. You can’t turn on a radio, or go to a mall without hearing Olivia Newton-John’s hit song, Physical."
-          }
-        ]
-      }
-    ])
-  }, [editor])
-
-  function handleItalic() {
-    editor?.chain().focus().toggleItalic().run();
-  }
-
-  function handleBold() {
-    editor?.chain().focus().toggleBold().run();
-  }
-
-  function handleBulletList() {
-    editor?.chain().focus().toggleBulletList().run();
-  }
-  return (
-    <>
-    <button className={`rounded-md border-[1px] p-2 ${editor?.isActive('italic') ? 'bg-white text-black' : 'border-white'}`} onClick={handleItalic}>Italic</button>
-    <button className={`rounded-md border-[1px] p-2 ${editor?.isActive('bold') ? 'bg-white text-black' : 'border-white'}`} onClick={handleBold}>Bold</button>
-    <button className={`rounded-md border-[1px] p-2 ${editor?.isActive('bulletList') ? 'bg-white text-black' : 'border-white'}`} onClick={handleBulletList}>Bullet List</button>
-    <EditorContent editor={editor} className="prose prose-sm text-white"/>
-    <p>{editorContent}</p>
-    </>
+export default function SubmitPage() {
+  return(
+    <SubmitPostPage />
   )
 }
