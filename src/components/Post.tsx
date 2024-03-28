4bf15af03224d90a294bc643e95bd875581d235d
LucioFurnari <luciofurnari99@gmail.com>
2024-03-28 20:25:12 -0300

    Change component to be client side, add tiptap generate html and parse html

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,17 +1,34 @@
+'use client'
+
+import { useMemo } from "react";
 import CommentsContainer from "./CommentsContainer";
+import { JSONContent, generateHTML } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import parse from 'html-react-parser';
 
 export default function PostPage (props: {
   title: string,
   author: string,
   text: string,
-  id: string
+  body?: JSONContent,
+  id: string,
 },
 ) {
+  const output = useMemo(() => {
+    if (props.body) {
+      return generateHTML(props.body, [
+        StarterKit
+      ])
+    }
+  }, [props.body]);
+
   return (
   <main className="  px-96">
     <h1 className=" text-4xl">{props.title}</h1>
     <p>Author: <span>{props.author}</span></p>
-    <p className="my-10">{props.text}</p>
+    <div className="my-10 prose prose-sm text-white">
+      {output && parse(output)}
+    </div>
     <CommentsContainer postId={props.id}/>
   </main>
   )
