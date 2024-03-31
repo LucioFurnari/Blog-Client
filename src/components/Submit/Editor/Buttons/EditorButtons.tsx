295f288436304a0fdb95269f39c58141c8ac399c
LucioFurnari <luciofurnari99@gmail.com>
2024-03-31 15:52:50 -0300

    Import Editor class and change the component

--- src/components/Submit/Editor/Buttons/EditorButtons.tsx
+++ src/components/Submit/Editor/Buttons/EditorButtons.tsx
@@ -1,8 +1,10 @@
+import { Editor } from "@tiptap/react";
 
-export default function EditorButton(props: { name: string, }) {
+export default function EditorButton(props: { name: string, editor: Editor | null, handleFunction: () => void }) {
+  const { name, editor, handleFunction } = props;
   return (
-    <button>
-
+    <button className={`rounded-md border-[1px] p-2 ${editor?.isActive('italic') ? 'bg-white text-black' : 'border-white'}`} onClick={handleFunction}>
+      {name}
     </button>
   )
 }
