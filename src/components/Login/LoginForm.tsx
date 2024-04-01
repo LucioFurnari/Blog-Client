ad5a073d245af10accb7185d3233985a7715444e
LucioFurnari <luciofurnari99@gmail.com>
2024-04-01 13:51:03 -0300

    Create Login folder and mover component

---
+++ src/components/Login/LoginForm.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { UserContext } from "@/app/context/UserContext";
+import { useContext } from "react";
+import { handleLogin } from "../actions";
+
+export default function LoginForm () {
+  const { listenStorageChange } = useContext(UserContext)
+
+  function handleSubmit (formData: FormData ) {
+    handleLogin(formData)
+    listenStorageChange()
+  }
+
+  return (
+    <form action={handleSubmit}>
+      <label htmlFor="username">Username</label>
+      <input className="text-black" name="username" id="username" type="text" placeholder="Username"></input>
+      <input className="text-black" name="password" type="password" placeholder="Password"></input>
+      <button type="submit">Login</button>
+    </form>
+  )
+}
