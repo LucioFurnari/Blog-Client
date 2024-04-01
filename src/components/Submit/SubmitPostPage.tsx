71b731433c8606e00b02e240ce18ef3149133527
LucioFurnari <luciofurnari99@gmail.com>
2024-04-01 11:43:01 -0300

    Move component to Submit folder

---
+++ src/components/Submit/SubmitPostPage.tsx
@@ -0,0 +1,9 @@
+import CreatePostForm from "./CreatePostForm"
+
+export default function SubmitPostPage() {
+  return(
+    <main className="flex flex-row justify-center">
+      <CreatePostForm />
+    </main>
+  )
+}
