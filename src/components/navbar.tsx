9c2caf13e60ef1f2b11a91f7a6962fb21b685bd1
LucioFurnari <luciofurnari99@gmail.com>
2024-04-03 23:48:35 -0300

    minor changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Link from "next/link";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { deleteToken } from "./actions";
 import { UserContext } from "@/app/context/UserContext";
 
@@ -14,7 +14,7 @@ export default function Navbar () {
   }
 
   return (
-    <nav className=" flex justify-around">
+    <nav className="py-6 flex justify-around items-center">
       <Link className=" p-6 hover:text-cyan-300" href={"/"}>Home</Link>
       <div className="flex flex-row">
       
@@ -22,15 +22,16 @@ export default function Navbar () {
         <>
         {user &&
         <>
-          <p className="p-6">{user}</p> 
-          <button onClick={ handleDeleteUser }>Logout</button>
+          <p className="p-4">{user}</p>
+          <Link className="p-4" href={"/submit"}>Create post</Link>
+          <button className="p-4" onClick={ handleDeleteUser }>Logout</button>
         </>
         }
         {
         !user &&
         <>
-        <Link className="p-6 hover:text-cyan-300"href={"/login"}>Login</Link>
-        <Link className="p-6 hover:text-cyan-300" href={"/signup"}>Signup</Link>
+        <Link className="p-4 hover:text-cyan-300"href={"/login"}>Login</Link>
+        <Link className="p-4 hover:text-cyan-300" href={"/signup"}>Signup</Link>
         </>
         }
         </>
