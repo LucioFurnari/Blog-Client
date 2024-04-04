683a14973be932c6466738c2641ee0b2ae1ec027
LucioFurnari <luciofurnari99@gmail.com>
2024-04-04 19:32:11 -0300

    Import new component, create new buttons

--- src/components/Submit/Editor/Buttons/ButtonsContainer.tsx
+++ src/components/Submit/Editor/Buttons/ButtonsContainer.tsx
@@ -1,5 +1,5 @@
 import { Editor } from "@tiptap/react"
-import EditorButton from "./EditorButtons"
+import { EditorButton, EditorHeaderButton } from "./EditorButtons"
 
 export default function ButtonsContainer(props: { editor: Editor | null}) {
   const { editor } = props;
@@ -20,12 +20,37 @@ export default function ButtonsContainer(props: { editor: Editor | null}) {
     editor?.chain().focus().toggleCode().run();
   }
 
+  function handleBlockqoute() {
+    editor?.chain().focus().toggleBlockquote().run();
+  }
+
+  function handleCodeBlock() {
+    editor?.chain().focus().toggleCodeBlock().run();
+  }
+
+  function handleHeaderH1() {
+    editor?.chain().focus().toggleHeading({ level: 1}).run();
+  }
+
+  function handleHeaderH2() {
+    editor?.chain().focus().toggleHeading({ level: 2}).run();
+  }
+
+  function handleHeaderH3() {
+    editor?.chain().focus().toggleHeading({ level: 3}).run();
+  }
+
   return (
     <div>
       <EditorButton name={'Italic'} activeClass={'italic'} editor={editor} handleFunction={handleItalic} />
       <EditorButton name={'Bold'} activeClass={'bold'} editor={editor} handleFunction={handleBold} /> 
       <EditorButton name={'Bullet List'} activeClass={'bulletList'} editor={editor} handleFunction={handleBulletList} />
       <EditorButton name={'Code'} activeClass={'code'} editor={editor} handleFunction={handleCode}/>
+      <EditorButton name={'Blockqoute'} activeClass={'blockquote'} editor={editor} handleFunction={handleBlockqoute} />
+      <EditorButton name={'Codeblock'} activeClass={'codeBlock'} editor={editor} handleFunction={handleCodeBlock}/>
+      <EditorHeaderButton name={'H1'} activeClass={'heading'} editor={editor} handleFunction={handleHeaderH1} headerLevel={1}/>
+      <EditorHeaderButton name={'H2'} activeClass={'heading'} editor={editor} handleFunction={handleHeaderH2} headerLevel={2}/>
+      <EditorHeaderButton name={'H3'} activeClass={'heading'} editor={editor} handleFunction={handleHeaderH3} headerLevel={3}/>
     </div>
   )
 }
