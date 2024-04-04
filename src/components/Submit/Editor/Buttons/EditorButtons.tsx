4453f868f1fcbbccb6f34d0035dea815c68f1430
LucioFurnari <luciofurnari99@gmail.com>
2024-04-04 15:39:26 -0300

    Add new prop, to fix isActive class

--- src/components/Submit/Editor/Buttons/EditorButtons.tsx
+++ src/components/Submit/Editor/Buttons/EditorButtons.tsx
@@ -1,9 +1,9 @@
 import { Editor } from "@tiptap/react";
 
-export default function EditorButton(props: { name: string, editor: Editor | null, handleFunction: () => void }) {
-  const { name, editor, handleFunction } = props;
+export default function EditorButton(props: { name: string, editor: Editor | null, handleFunction: () => void, activeClass: string }) {
+  const { name, editor, handleFunction, activeClass } = props;
   return (
-    <button className={`rounded-md border-[1px] p-2 ${editor?.isActive('italic') ? 'bg-white text-black' : 'border-white'}`} onClick={handleFunction}>
+    <button className={`rounded-md border-[1px] p-2 ${editor?.isActive(activeClass) ? 'bg-white text-black' : 'border-white'}`} onClick={handleFunction}>
       {name}
     </button>
   )
