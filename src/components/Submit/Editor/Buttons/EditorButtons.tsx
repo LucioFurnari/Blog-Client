fe34af2fe0c49395c85d1db817aac622a91cc769
LucioFurnari <luciofurnari99@gmail.com>
2024-04-04 19:32:36 -0300

    Create new components for the headers, remove export default

--- src/components/Submit/Editor/Buttons/EditorButtons.tsx
+++ src/components/Submit/Editor/Buttons/EditorButtons.tsx
@@ -1,9 +1,20 @@
 import { Editor } from "@tiptap/react";
 
-export default function EditorButton(props: { name: string, editor: Editor | null, handleFunction: () => void, activeClass: string }) {
+export function EditorButton(props: { name: string, editor: Editor | null, handleFunction: () => void, activeClass: string }) {
   const { name, editor, handleFunction, activeClass } = props;
+
   return (
     <button className={`rounded-md border-[1px] p-2 ${editor?.isActive(activeClass) ? 'bg-white text-black' : 'border-white'}`} onClick={handleFunction}>
+      {name}
+    </button>
+  )
+}
+
+export function EditorHeaderButton(props: { name: string, editor: Editor | null, handleFunction: () => void, activeClass: string, headerLevel: number}) {
+  const { name, editor, handleFunction, activeClass, headerLevel} = props;
+
+  return(
+    <button className={`rounded-md border-[1px] p-2 ${editor?.isActive(activeClass, { level: headerLevel}) ? 'bg-white text-black' : 'border-white'}`} onClick={handleFunction}>
       {name}
     </button>
   )
