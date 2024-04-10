cdb381f8a74bbf42e46313fa432f727b46e68020
LucioFurnari <luciofurnari99@gmail.com>
2024-04-09 22:26:37 -0300

    Fix attribute name in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -47,7 +47,7 @@ export default function Navbar () {
         </div>
         <div className=" md:hidden">
         <button onClick={handleBurgerMenu} className="outline-none mobile-menu-button">
-          <svg className={`${hamburgerMenu && 'border-2 border-gray-400 rounded-md'} w-6 h-6 text-white`} x-show="!showMenu" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+          <svg className={`${hamburgerMenu && 'border-2 border-gray-400 rounded-md'} w-6 h-6 text-white`} x-show="!showMenu" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
             <path d="M4 6h16M4 12h16M4 18h16"></path>
           </svg>
         </button>
