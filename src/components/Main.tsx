8724f05a69827a21c2e8f0e8cf372c19b3cbd3d1
LucioFurnari <luciofurnari99@gmail.com>
2024-04-09 23:24:02 -0300

    Remove h1

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,7 +6,6 @@ export default async function Main () {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1>This is the home</h1>
       <PostCardContainer posts={posts} />
     </main>
   )
