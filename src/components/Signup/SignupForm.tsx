75b0117d85859a8e76b952492108e6718905e884
LucioFurnari <luciofurnari99@gmail.com>
2024-04-10 21:59:17 -0300

    Change style of the component

--- src/components/Signup/SignupForm.tsx
+++ src/components/Signup/SignupForm.tsx
@@ -20,11 +20,41 @@ export default function SignupForm () {
   }
 
   return (
-    <form action={handleSubmit}>
-      <input className="text-black" type="text" name="username" placeholder="username"></input>
-      <input className="text-black" type="text" name="password" placeholder="password"></input>
-      <input className="text-black" type="email" name="email" placeholder="email"></input>
-      <button type="submit">SignUp</button>
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form className="space-y-6" action={handleSubmit}>
+      <label htmlFor="username" className="block text-sm font-medium leading-6">
+        Username
+      </label>
+      <input 
+        className="mt-2 block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+        name="username"
+        id="username"
+        type="text"
+      />
+      <label htmlFor="password" className="block text-sm font-medium leading-6">
+        Password
+      </label>
+      <input
+        className="mt-2 block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        name="password"
+        id="password"
+        type="text"
+      />
+      <label htmlFor="email" className="block text-sm font-medium leading-6">
+        Email
+      </label>
+      <input
+        className="mt-2 block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        name="email"
+        id="email"
+        type="email"
+      />
+      <button
+        className="w-full rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        type="submit">
+          Register
+        </button>
     </form>
+    </div>
   )
 }
