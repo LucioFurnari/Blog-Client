0c735dc94fbd47056e45d6b72fc1d5e64e5346b4
LucioFurnari <luciofurnari99@gmail.com>
2024-05-10 23:13:59 -0300

    Create postId state

--- src/app/context/UserContext.tsx
+++ src/app/context/UserContext.tsx
@@ -11,16 +11,15 @@ type  UserContent = {
   setIsLogged:(c: boolean) => void,
   listenStorageChange: () => void,
   deleteUser: () => void,
+  postId: string,
+  setPostId: (id: string) => void
 }
 export const UserContext = createContext<UserContent>({} as UserContent);
 
-export function UserProvider ({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export function UserProvider ({ children }: Readonly<{children: React.ReactNode;}>) {
   const [isLogged, setIsLogged] = useState(false);
   const [user, setUser] = useState(null);
+  const [postId, setPostId] = useState('');
 
   useEffect(() => {
     listenStorageChange()
@@ -31,15 +30,15 @@ export function UserProvider ({
     try {
       const token = await getToken();
       if (token !== undefined) {
-      const res = await getSession(token);
-      //todo: Change this
-      if (typeof res === 'undefined') {
-        throw new Error('Server not working')
-      }
-      if (res.ok) {
-        const data = await res.json()
-        setUser(data.username)
-      }
+        const res = await getSession(token);
+        //todo: Change this
+        if (typeof res === 'undefined') {
+          throw new Error('Server not working')
+        }
+        if (res.ok) {
+          const data = await res.json()
+          setUser(data.username)
+        }
       }
     } catch (error) {
       console.error(error)
@@ -51,7 +50,7 @@ export function UserProvider ({
   }
 
   return (
-    <UserContext.Provider value={{user, setUser, setIsLogged, isLogged, listenStorageChange, deleteUser}}>
+    <UserContext.Provider value={{user, setUser, setIsLogged, isLogged, listenStorageChange, deleteUser, postId, setPostId}}>
       <>
         {children}
       </>
