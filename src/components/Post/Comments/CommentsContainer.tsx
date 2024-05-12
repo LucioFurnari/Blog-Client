3dafd76bdbec983ecd7e8db06a20764ec5788cd3
LucioFurnari <luciofurnari99@gmail.com>
2024-05-11 23:55:27 -0300

    Import useContext, and UserContext

--- src/components/Post/Comments/CommentsContainer.tsx
+++ src/components/Post/Comments/CommentsContainer.tsx
@@ -1,18 +1,21 @@
 'use client'
 
 import { CommentList } from "./CommentList";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { getNewComments } from "../../actions";
-
+import { UserContext } from "@/app/context/UserContext";
 
 export default function CommentsContainer (props: {postId: string, refreshComments: boolean}) {
-  const {postId, refreshComments} = props;
+  const { refreshComments } = props;
   const [comments, setComments] = useState([]);
+  const { postId } = useContext(UserContext);
 
   useEffect(() => {
     async function handleComments(postId: string) {
       const newComments = await getNewComments(postId);
-      setComments(newComments)
+      if(newComments !== undefined) {
+        setComments(newComments)
+      }
     }
     handleComments(postId);
   }, [postId, refreshComments])
@@ -35,12 +38,12 @@ export default function CommentsContainer (props: {postId: string, refreshCommen
 
   const group: GroupObject = {};
 
-  if (comments.length > 0) {
-    comments.forEach((comment: Comment) => {
-      group[String(comment.response_to)] ||= []; // Initialize to an empty array if undefined or null
-      group[String(comment.response_to)].push(comment);
-    });
-  }
+    if (comments.length > 0) {
+      comments.forEach((comment: Comment) => {
+        group[String(comment.response_to)] ||= []; // Initialize to an empty array if undefined or null
+        group[String(comment.response_to)].push(comment);
+      });
+    }
 
   const rootComments = group['null']
 
@@ -48,7 +51,7 @@ export default function CommentsContainer (props: {postId: string, refreshCommen
     <>
     {
       rootComments &&
-      <CommentList postId={postId} comments={rootComments} group={group} getReplies={getReplies}/>
+      <CommentList comments={rootComments} group={group} getReplies={getReplies}/>
     }
     </>
   )
