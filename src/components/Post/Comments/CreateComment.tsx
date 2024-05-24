8655e79978e6e3ddfc310919a235e7d7f32020e3
LucioFurnari <luciofurnari99@gmail.com>
2024-05-23 23:37:07 -0300

    Change set active value, add setActive as prop

--- src/components/Post/Comments/CreateComment.tsx
+++ src/components/Post/Comments/CreateComment.tsx
@@ -7,15 +7,15 @@ export default function CreateComment(props: { postId: string, setRefresh: (valu
   const [activeComment, setActiveComment] = useState(false);
 
   function handleActiveComment() {
-    setActiveComment((value: boolean) => !value)
+    setActiveComment(true)
   }
 
   return (
     <section>
-      <button className=" bg-gray-600 rounded-md p-2 px-4 hover:bg-gray-500" onClick={handleActiveComment}>Create comment</button>
+      <button className="p-2 px-4 bg-sky-600 hover:bg-sky-500 rounded-md" onClick={handleActiveComment}>Create comment</button>
       {
         activeComment &&
-        <CreateCommentForm postId={postId} setRefresh={setRefresh} />
+        <CreateCommentForm postId={postId} setRefresh={setRefresh} setActiveComment={setActiveComment}/>
       }
     </section>
   )
